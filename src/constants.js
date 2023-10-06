79d5de14aed14ca27c212ff75c378a06d8a5490e
Binaya Sharma <binayashrma11@gmail.com>
2023-10-06 14:28:53 -0500

    changed backend api url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,6 @@
-export const API_BASE_URL = 'http://localhost:8080/api';
+// export const API_BASE_URL = 'http://localhost:8080/api';
+
+export const API_BASE_URL = 'http://18.222.230.229:8080/api';
 export const LOGIN_URL = API_BASE_URL + '/login';
 export const SIGNUP_URL = API_BASE_URL + '/users';
 export const TRANSACTION_URL = API_BASE_URL + '/transactions';
